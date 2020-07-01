15a204caabffa9fc0a732d07929fe8efd150db82
Sergei Malyshev <xwzvm@yandex.ru>
2020-07-01 14:40:14 +0300

    Add current time class

---
+++ src/Delay/Time/Now.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace Tamer\Delay\Time;
+
+/**
+ * @author Sergei Malyshev <xwzvm@yandex.ru>
+ */
+final class Now implements InMicroseconds
+{
+    /**
+     * @inheritDoc
+     */
+    public function microseconds(): float
+    {
+        return (new Second((float) time()))->microseconds();
+    }
+}
